73adf20d5fa76701469b3abc1d6a23f38d864ef0
Adam Buczynski <adambuczynski@gmail.com>
2016-02-10 14:17:43 +1300

    1.1.3

--- release/meanie-angular-analytics.js
+++ release/meanie-angular-analytics.js
@@ -1,5 +1,5 @@
 /**
- * meanie-angular-analytics - v1.1.2 - 11-0-2016
+ * meanie-angular-analytics - v1.1.3 - 10-1-2016
  * https://github.com/meanie/angular-analytics
  *
  * Copyright (c) 2016 Adam Buczynski <me@adambuczynski.com>
